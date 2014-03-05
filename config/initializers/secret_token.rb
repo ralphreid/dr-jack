f1bc949adb0f8de58820eb61ea0950689d40711a
Ralph Reid <beresfordjunior@me.com>
2014-03-05 23:06:46 +0000

    installed rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrJack::Application.config.secret_key_base = '0b27633c0dd05f4abd0ef39e9d114de29427da4f8226a1dd5fcbb98d657748ded8cf7495bab495cd6c8bc348c05f358ef3bd8ec7efe113d060ae40878fa391d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrJack::Application.config.session_store :cookie_store, key: '_dr-jack_session'
